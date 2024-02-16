d5511023f2d6b6793104231e13fa834a24a08321
Ernest Khasanzhinov <ernest.khasanzhinov@workato.com>
2024-02-16 22:15:34 +0100

    add test checking

--- cmd/log_manager/main.go
+++ cmd/log_manager/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ernest_k/defi_stats/lib"
 	"github.com/ernest_k/defi_stats/liquidation_event"
+	"github.com/ernest_k/defi_stats/repositories/redis"
 	"github.com/joho/godotenv"
 )
 
@@ -25,7 +26,12 @@ func main() {
 		today := time.Now()
 		liquidation_event.LogManager(today)
 	}
-	// if arg1 == "3" {
-
-	// }
+	if arg1 == "3" {
+		n := int64(23)
+		redis.WriteLastProcessedBlock("er1", n)
+	}
+	if arg1 == "4" {
+		n1 := redis.GetLastProcessedBlock(1, "er1")
+		fmt.Println("latestBlock ", n1)
+	}
 }
